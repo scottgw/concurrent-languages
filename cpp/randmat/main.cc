314f202548d37941a779d8098de1f89f08b4d54d
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-03-21 21:39:22 -0300

    cpp-randmat done

--- cpp/randmat/main.cc
+++ cpp/randmat/main.cc
@@ -11,21 +11,40 @@
 #include <cstdio>
 #include <cstdlib>
 
-// TODO: function randmat
+void randmat(int nrows, int ncols, int s, int** matrix) {
+  srand(s);
+  for (int i = 0; i < nrows; i++) {
+    for (int j = 0; j < ncols; j++) {
+      matrix[i][j] = rand();
+    }
+  }
+}
 
 int main(int argc, char** argv) {
-  assert(argc == 4);
-  int nrows = atoi(argv[1]),
-      ncols = atoi(argv[2]),
-      s = atoi(argv[3]);
-  srand(s);
+  int nrows, ncols, s;
+
+  scanf("%d%d%d", &nrows, &ncols, &s);
+
+  int** matrix = new int* [nrows];
+  for (int i = 0; i < nrows; i++) {
+    matrix[i] = new int[ncols];
+  }
+
+  randmat(nrows, ncols, s, matrix);
+
   printf("%d %d\n", nrows, ncols);
   for (int i = 0; i < nrows; i++) {
     for (int j = 0; j < ncols; j++) {
-      printf("%d ", rand());
+      printf("%d ", matrix[i][j]);
     }
     printf("\n");
   }
   printf("\n");
+
+  for (int i = 0; i < nrows; i++) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
+
   return 0;
 }
