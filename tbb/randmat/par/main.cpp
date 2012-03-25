3aec74a36bfb30629ed27b86c23a05998678deb6
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-03-25 20:43:27 -0300

    tbb-randmat-par done

--- tbb/randmat/par/main.cpp
+++ tbb/randmat/par/main.cpp
@@ -11,13 +11,45 @@
 #include <cstdio>
 #include <cstdlib>
 
-void randmat(int nrows, int ncols, int s, int** matrix) {
-  srand(s);
-  for (int i = 0; i < nrows; i++) {
-    for (int j = 0; j < ncols; j++) {
-      matrix[i][j] = rand();
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range.h"
+
+using namespace tbb;
+
+class RandMatCol {
+public: 
+  void operator() ( const blocked_range<size_t>& r ) const { 
+    for ( size_t j = r.begin(); j != r.end(); ++j ) {
+      _matrix[_row][j] = rand();
     }
   }
+  RandMatCol(int row, int** matrix): _row(row), _matrix(matrix) {}
+
+private:
+  int _row;
+  int** _matrix;
+};
+
+
+class RandMatRow {
+public: 
+  void operator() ( const blocked_range<size_t>& r ) const { 
+    for ( size_t i = r.begin(); i != r.end(); ++i ) {
+      parallel_for(blocked_range<size_t>(0, _ncols),
+          RandMatCol(i, _matrix), auto_partitioner());
+    }
+  }
+  RandMatRow(int ncols, int** matrix): _ncols(ncols), _matrix(matrix) {}
+
+private:
+  int _ncols;
+  int** _matrix;
+};
+
+void randmat(int nrows, int ncols, int s, int** matrix) {
+  srand(s);
+  parallel_for(blocked_range<size_t>(0, nrows),
+      RandMatRow(ncols, matrix), auto_partitioner());
 }
 
 int main(int argc, char** argv) {
