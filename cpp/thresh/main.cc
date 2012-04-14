7f028004d6e5bcb43386c3eb5baa55ca440f0b3a
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-04-14 16:28:17 -0300

    cpp-thresh done

--- cpp/thresh/main.cc
+++ cpp/thresh/main.cc
@@ -1,41 +1,85 @@
-/* randmat: random number generator
+/* thresh: histogram thresholding
  *
  * input:
+ *   matrix: the integer matrix to be thresholded
  *   nrows, ncols: number of rows and columns
- *   s: random number generation seed
+ *   percent: the percentage of cells to retain
  *
  * output:
- *   matrix: random nrows x ncols integer matrix
+ *   mask: a boolean matrix filled with true for cells that are kept
  */
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
 
-void randmat(int nrows, int ncols, int s, int** matrix) {
-  srand(s);
+#include <algorithm>
+
+using namespace std;
+
+void thresh(int nrows, int ncols, int** matrix, int percent, int** mask) {
+  int nmax = 0;
   for (int i = 0; i < nrows; i++) {
     for (int j = 0; j < ncols; j++) {
-      matrix[i][j] = rand();
+      nmax = max(nmax, matrix[i][j]);
     }
   }
+
+  int* histogram = new int[nmax + 1];
+
+  for (int i = 0; i < nrows; i++) {
+    for (int j = 0; j < ncols; j++) {
+      histogram[matrix[i][j]]++;
+    }
+  }
+
+  int count = (nrows * ncols * percent) / 100;
+
+  int prefixsum = 0;
+  int threshold = nmax;
+
+  for (int i = nmax; i >= 0 && prefixsum <= count; i--) {
+    prefixsum += histogram[i];
+    threshold = i;
+  }
+
+  for (int i = 0; i < nrows; i++) {
+    for (int j = 0; j < ncols; j++) {
+      mask[i][j] = matrix[i][j] >= threshold;
+    }
+  }
+
+  delete[] histogram;
 }
 
 int main(int argc, char** argv) {
-  int nrows, ncols, s;
+  int nrows, ncols, percent;
 
-  scanf("%d%d%d", &nrows, &ncols, &s);
+  scanf("%d%d", &nrows, &ncols);
 
   int** matrix = new int* [nrows];
   for (int i = 0; i < nrows; i++) {
     matrix[i] = new int[ncols];
   }
 
-  randmat(nrows, ncols, s, matrix);
+  int** mask = new int* [nrows];
+  for (int i = 0; i < nrows; i++) {
+    mask[i] = new int[ncols];
+  }
+
+  for (int i = 0; i < nrows; i++) {
+    for (int j = 0; j < ncols; j++) {
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+
+  scanf("%d", &percent);
+
+  thresh(nrows, ncols, matrix, percent, mask);
 
   printf("%d %d\n", nrows, ncols);
   for (int i = 0; i < nrows; i++) {
     for (int j = 0; j < ncols; j++) {
-      printf("%d ", matrix[i][j]);
+      printf("%d ", mask[i][j]);
     }
     printf("\n");
   }
@@ -45,6 +89,11 @@ int main(int argc, char** argv) {
     delete[] matrix[i];
   }
   delete[] matrix;
+
+  for (int i = 0; i < nrows; i++) {
+    delete[] mask[i];
+  }
+  delete[] mask;
 
   return 0;
 }
