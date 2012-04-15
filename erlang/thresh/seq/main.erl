90419520ff6212f0e0a3a591d9c4c9e545ab9de1
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-04-15 17:54:51 -0300

    erlang-thresh-seq max_matrix

--- erlang/thresh/seq/main.erl
+++ erlang/thresh/seq/main.erl
@@ -13,7 +13,31 @@
 -module(main).
 -export([main/0]).
 
-thresh(Nrows, Ncols, Matrix, Percent) -> [Ncols, Nrows, Matrix, Percent].
+max(A, B) ->
+  if A > B -> A;
+    true -> B
+  end.
+
+max_vector([]) -> 0;
+max_vector([Head | Rest]) -> max(Head, max_vector(Rest)).
+
+max_matrix(_, _, []) -> 0;
+max_matrix(_, _, [Head | Rest]) -> max(max_vector(Head),
+    max_matrix(0, 0, Rest)).
+
+fill_histogram(Nrows, Ncols, Matrix, Nmax) -> [0].
+
+get_threshold(Histogram, Count) -> 0.
+
+filter(Nrows, Ncols, Matrix, Threshold) -> [[]].
+
+thresh(Nrows, Ncols, Matrix, Percent) ->
+  Nmax = max_matrix(Nrows, Ncols, Matrix),
+  Histogram = fill_histogram(Nrows, Ncols, Matrix, Nmax),
+  Count = (Nrows * Ncols * Percent) / 100,
+  Threshold = get_threshold(Histogram, Count),
+  Mask = filter(Nrows, Ncols, Matrix, Threshold),
+  Mask.
 
 read_vector(0) -> [];
 read_vector(Ncols) -> {ok, [Value]} = io:fread("", "~d"),
