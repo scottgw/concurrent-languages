780b8c209e2faad3a133cbcb079888f5779a8416
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-04-18 16:07:26 -0300

    tbb-thresh-par pause

--- tbb/thresh/par/main.cc
+++ tbb/thresh/par/main.cc
@@ -14,28 +14,31 @@
 
 #include <algorithm>
 
+#include "tbb/parallel_for.h"
 #include "tbb/parallel_reduce.h"
 #include "tbb/blocked_range.h"
 
 using namespace std;
 
 typedef tbb::blocked_range<size_t> range;
+typedef function<int (int, int)> Operator;
 
-struct sOpMax{
-  int operator()(const int& x, const int& y) const {
-    return max(x, y);
-  }
-};
+int op_max(int x, int y) {
+  return max(x, y);
+}
 
-template <class Operator>
-int reduce2d(int nrows, int ncols, const vector<vector<int>>& matrix,
-    Operator op) {
+int op_sum(int x, int y) {
+  return x + y;
+}
+
+int reduce2d_with_filter(int nrows, int ncols,
+    const vector<vector<int>>& matrix, Operator aggregator, Operator op) {
   return tbb::parallel_reduce(
       range(0, nrows), 0,
       [=](range r, int partial_value)->int {
         int result = partial_value;
         for (size_t i = r.begin(); i != r.end(); i++) {
-          result = op(result, tbb::parallel_reduce(
+          result = aggregator(result, tbb::parallel_reduce(
             range(0, nrows), 0,
             [=](range r, int partial_value)->int {
               int result = partial_value;
@@ -44,25 +47,47 @@ int reduce2d(int nrows, int ncols, const vector<vector<int>>& matrix,
               }
               return result;
             },
-            op));
+            aggregator));
         }
         return result;
       },
-      op);
+      aggregator);
+}
+
+int reduce2d(int nrows, int ncols, const vector<vector<int>>& matrix,
+    Operator op) {
+  return reduce2d_with_filter(nrows, ncols, matrix, op, op);
 }
 
 void thresh(int nrows, int ncols, const vector<vector<int>>& matrix,
     int percent, vector<vector<int>>* mask) {
 
-  int nmax = reduce2d(nrows, ncols, matrix, sOpMax());
+  int nmax = reduce2d(nrows, ncols, matrix, op_max);
 
   vector<int> histogram(nmax + 1, 0);
 
-  for (int i = 0; i < nrows; i++) {
-    for (int j = 0; j < ncols; j++) {
-      histogram[matrix[i][j]]++;
-    }
+  tbb::parallel_for(
+      range(0, nrows),
+      [&](range r) {
+        for (size_t i = r.begin(); i != r.end(); ++i) {
+          histogram[i] = reduce2d_with_filter(nrows, ncols, matrix, op_sum,
+            [=](int acc, int value) {
+              printf("sum[%d](%d, %d)\n", i, acc, value);
+              return acc + (value == i);
+            });
+        }
+      });
+
+  for (int i = 0; i < nmax + 1; i++) {
+    printf("%d ", histogram[i]);
   }
+  printf("\n");
+
+  //for (int i = 0; i < nrows; i++) {
+    //for (int j = 0; j < ncols; j++) {
+      //histogram[matrix[i][j]]++;
+    //}
+  //}
 
   int count = (nrows * ncols * percent) / 100;
 
