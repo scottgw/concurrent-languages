e91aa3e057cb1ae4a14e218915d2f323c9c0f47f
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-04-30 16:54:08 -0300

    erlang-winnow-seq done

--- erlang/winnow/seq/main.erl
+++ erlang/winnow/seq/main.erl
@@ -1,28 +1,61 @@
 %
-% randmat: random number generation
+% winnow: weighted point selection
 %
 % input:
+%   matrix: an integer matrix, whose values are used as masses
+%   mask: a boolean matrix showing which points are eligible for
+%     consideration
 %   nrows, ncols: the number of rows and columns
-%   s: the seed
+%   nelts: the number of points to select
 %
 % output:
-%   matrix: a nrows x ncols integer matrix
+%   points: a vector of (x, y) points
 %
 
 -module(main).
 -export([main/0]).
--define(INT_MAX,2147483647).
 
-randvet(0) -> [];
-randvet(Ncols) -> [random:uniform(?INT_MAX) | randvet(Ncols - 1)].
+get_values_vector(_, _, [], []) -> [];
+get_values_vector(Line, Col, [ValuesHead | ValuesTail], [
+    MaskHead | MaskTail]) ->
+  Rest = get_values_vector(Line, Col + 1, ValuesTail, MaskTail),
+  if MaskHead == 1 -> [
+        {ValuesHead, {Line, Col}} | Rest];
+    true -> Rest
+  end.
 
-randmat_impl(0, _) -> [];
-randmat_impl(Nrows, Ncols) -> [randvet(Ncols) | randmat_impl(Nrows - 1, Ncols)].
+get_values(_, [], []) -> [];
+get_values(Line, [MatrixHead | MatrixTail], [MaskHead | MaskTail]) ->
+  get_values_vector(Line, 0, MatrixHead, MaskHead) ++ get_values(
+    Line + 1, MatrixTail, MaskTail).
 
-randmat(Nrows, Ncols, S) -> random:seed(S, S, S),
-  randmat_impl(Nrows, Ncols).
+drop(L, 0) -> L;
+drop([_ | T], I) -> drop (T, I - 1).
+
+get_points(0, _, _) -> [];
+get_points(Nelts, [{_, {I, J}} | Tail], Chunk) ->
+  [ {I, J} | get_points(Nelts - 1, drop(Tail, Chunk - 1), Chunk)].
+
+winnow(_, _, Matrix, Mask, Nelts) ->
+  Values = get_values(0, Matrix, Mask),
+  Sorted = lists:sort(Values),
+  N = length(Sorted),
+  Chunk = N div Nelts,
+  Points = get_points(Nelts, Sorted, Chunk),
+  [Points].
+
+read_vector(0) -> [];
+read_vector(Ncols) -> {ok, [Value]} = io:fread("", "~d"),
+  [ Value | read_vector(Ncols - 1)].
+
+read_matrix(0, _) -> [];
+read_matrix(Nrows, Ncols) -> [read_vector(Ncols) |
+    read_matrix(Nrows - 1, Ncols)].
 
 main() ->
-  {ok, [Nrows, Ncols, S]} = io:fread("","~d~d~d"),
-  io:format("~w~n\n", [randmat(Nrows, Ncols, S)]).
+  {ok, [Nrows, Ncols]} = io:fread("","~d~d"),
+  Matrix = read_matrix(Nrows, Ncols),
+  Mask = read_matrix(Nrows, Ncols),
+  {ok, [Nelts]} = io:fread("", "~d"),
+  io:format("~w~n\n", [winnow(Nrows, Ncols, Matrix, Mask, Nelts)]).
 
