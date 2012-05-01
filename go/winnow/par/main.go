e0d17d9589cc0563ea757ccb9ca7c8a2ca19970c
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-01 14:38:08 -0300

    go-winnow-par done

--- go/winnow/par/main.go
+++ go/winnow/par/main.go
@@ -16,7 +16,6 @@ package main
 
 import (
   "fmt"
-  "sort"
 )
 
 type Point struct {
@@ -43,40 +42,118 @@ func (p Points) Less(i, j int) bool {
   return p[i].j < p[j].j;
 }
 
+func reduce2d_worker(n int, array []int, op func(acc, x int) int,
+    start_value int, result chan int) {
+  res := start_value;
+  for i := 0; i < n; i++ {
+    res = op(res, array[i]);
+  }
+  result <- res;
+}
+
+func reduce2d(nrows, ncols int, matrix [][]int,
+    aggregator func(acc, x int) int, aggregator_start_value int,
+    op func(acc, x int) int, op_start_value int) int {
+  response := make(chan int);
+  for i := 0; i < nrows; i++ {
+    go reduce2d_worker(ncols, matrix[i], op, op_start_value, response);
+  }
+
+  results := make([]int, nrows);
+  for i := 0; i < nrows; i++ {
+    results[i] = <-response
+  }
+
+  go reduce2d_worker(nrows, results, aggregator, aggregator_start_value,
+      response);
+
+  return <-response;
+}
+
+func sum(a, b int) int {
+  return a + b;
+}
+
+func split_worker(index int, op func(index int), done chan bool) {
+  op(index);
+  done <- true;
+}
+  
+func split(begin, end int, op func(index int)) {
+  done := make(chan bool);
+  for i := begin; i < end; i++ {
+    go split_worker(i, op, done)
+  }
+  for i := begin; i < end; i++ {
+    <-done;
+  }
+}
+
+func get_count_func(ncols int, matrix, mask [][]int, result chan Point) (
+    func(index int)) {
+  return func(index int) {
+    for j := 0; j < ncols; j++ {
+      if mask[index][j] == 1 {
+        result <- Point{matrix[index][j], index, j};
+      }
+    }
+  };
+}
+
+func sort_impl(begin, end int, values Points) {
+  if (begin + 1 >= end) {
+    return;
+  }
+  var pivot_index int = (begin + end) / 2;
+  values.Swap(end - 1, pivot_index);
+  spot := begin;
+  for i := begin; i < end; i++ {
+    if values.Less(i, end - 1) {
+      values.Swap(i, spot);
+      spot++;
+    }
+  }
+  values.Swap(spot, end - 1);
+  pivot_index = spot;
+
+  split(0, 2, func(index int) {
+      if index == 0 {
+        sort_impl(begin, pivot_index, values);
+      } else {
+        sort_impl(pivot_index + 1, end, values);
+      }
+    });
+}
+
+func sort(n int, values []Point) {
+  sort_impl(0, n, values);
+}
+
 func winnow(nrows, ncols int, matrix, mask [][]int, nelts int) []Point {
   var n = 0;
 
-  for i := 0; i < nrows; i++ {
-    for j := 0; j < ncols; j++ {
-      if mask[i][j] == 1 {
-        n++;
-      }
-    }
-  }
+  n = reduce2d(nrows, ncols, mask, sum, 0, sum, 0);
 
   var points, values Points;
   points = make(Points, n);
   values = make(Points, n);
 
-  var count = 0;
-  for i := 0; i < nrows; i++ {
-    for j := 0; j < ncols; j++ {
-      if mask[i][j] == 1 {
-        values[count] = Point{matrix[i][j], i, j};
-        count++;
-      }
-    }
+  result := make(chan Point, n);
+  split(0, nrows, get_count_func(ncols, matrix, mask, result));
+
+  for i := 0; i < n; i++ {
+    values[i] = <-result;
   }
-  
-  sort.Sort(values);
+
+  sort(n, values);
 
   var total = len(values);
   var chunk int = total / nelts;
 
-  for i := 0; i < nelts; i++ {
-    var index = i * chunk;
-    points[i] = values[index];
-  }
+  split(0, nelts, func(i int) {
+      var index = i * chunk;
+      points[i] = values[index];
+    });
 
   return points;
 }
