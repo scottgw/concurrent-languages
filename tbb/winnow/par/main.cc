b14d104d88641cd1c38e1c22b420a5719926d4a7
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-01 19:54:08 -0300

    tbb-winnow-par done

--- tbb/winnow/par/main.cc
+++ tbb/winnow/par/main.cc
@@ -18,29 +18,50 @@
 #include <iostream>
 #include <vector>
 
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+#include "tbb/parallel_sort.h"
+
 using namespace std;
+using namespace tbb;
+
+typedef blocked_range<size_t> range;
 
 void winnow(int nrows, int ncols, const vector<vector<int> >& matrix,
     const vector<vector<int> >& mask, int nelts,
     vector<pair<int, int> >* points) {
   vector<pair<int, pair<int, int> > > values;
-  for (int i = 0; i < nrows; i++) {
-    for (int j = 0; j < ncols; j++) {
-      if (mask[i][j]) {
-        values.push_back(make_pair(matrix[i][j], make_pair(i, j)));
-      }
-    }
-  }
-  sort(values.begin(), values.end());
+
+  parallel_for(
+      range(0, nrows),
+      [&](range r) {
+        for (size_t i = r.begin(); i != r.end(); ++i) {
+          tbb::parallel_for(
+            range(0, ncols),
+            [&](range s) {
+              for (size_t j = s.begin(); j != s.end(); ++j) {
+                if (mask[i][j]) {
+                  values.push_back(make_pair(matrix[i][j],
+                      make_pair(i, j)));
+                }
+              }
+            });
+        }
+      });
+
+  parallel_sort(values.begin(), values.end());
 
   size_t n = values.size();
   size_t chunk = n / nelts;
 
-  for (int i = 0; i < nelts; i++) {
-    int index = i * chunk;
-    (*points)[i] = values[index].second;
-  }
-
+  parallel_for(
+      range(0, nelts),
+      [&](range r) {
+        for(size_t i = r.begin(); i != r.end(); ++i) {
+          int index = i * chunk;
+          (*points)[i] = values[index].second;
+        }
+      });
 }
 
 void read_matrix(int nrows, int ncols, vector<vector<int> >* matrix) {
