eea5d44578d100b0b9698c10fd759788fbd908a4
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-01 13:12:12 -0300

    go-winnow-seq start

---
+++ go/winnow/seq/main.go
@@ -0,0 +1,49 @@
+/*
+ * randmat: random number generation
+ *
+ * input:
+ *   nrows, ncols: the number of rows and columns
+ *   s: the seed
+ *
+ * output:
+ *   martix: a nrows x ncols integer matrix
+ *
+ */
+package main
+
+import (
+  "fmt"
+  "rand"
+)
+
+func randmat(nrows, ncols, s int) [][]int {
+  var matrix [][]int;
+  matrix = make([][]int, nrows);
+  for i := 0; i < nrows; i++ {
+    matrix[i] = make([]int, ncols);
+  }
+  rand.Seed(int64(s));
+  for i := 0; i < nrows; i++ {
+    for j := 0; j < ncols; j++ {
+      matrix[i][j] = rand.Int();
+    }
+  }
+  return matrix;
+}
+
+func main() {
+  var nrows, ncols, s int;
+  var matrix [][]int;
+
+  fmt.Scanf("%d%d%d", &nrows, &ncols, &s);
+
+  matrix = randmat(nrows, ncols, s);
+
+  for i := 0; i < nrows; i++ {
+    for j := 0; j < ncols; j++ {
+      fmt.Printf("%d ", matrix[i][j]);
+    }
+    fmt.Printf("\n");
+  }
+  fmt.Printf("\n");
+}
