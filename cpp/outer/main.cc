72f9ba5983eed4e471835053152f4977e501d01b
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-01 21:14:07 -0300

    cpp-outer done

--- cpp/outer/main.cc
+++ cpp/outer/main.cc
@@ -1,50 +1,81 @@
-/* randmat: random number generator
+/* outer: outer product
  *
  * input:
- *   nrows, ncols: number of rows and columns
- *   s: random number generation seed
+ *   points: a vector of (x, y) points
+ *   nelts: the number of points
  *
  * output:
- *   matrix: random nrows x ncols integer matrix
+ *   matrix: a real matrix, whose values are filled with inter-point
+ *     distances
+ *   vec: a real vector, whose values are filled with origin-to-point
+ *     distances
  */
 #include <cassert>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
 
-void randmat(int nrows, int ncols, int s, int** matrix) {
-  srand(s);
-  for (int i = 0; i < nrows; i++) {
-    for (int j = 0; j < ncols; j++) {
-      matrix[i][j] = rand();
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double sqr(double x) {
+  return x * x;
+}
+
+double distance(const pair<int, int>& x, const pair<int, int>& y) {
+  return sqrt(sqr(x.first - y.first) + sqr(x.second - y.second));
+}
+
+void outer(int nelts, const vector<pair<int, int> > & points,
+    vector<vector<double> >* matrix, vector<double>* vec) {
+  for (int i = 0; i < nelts; i++) {
+    double nmax = -1;
+    for (int j = 0; j < nelts; j++) {
+      if (i != j) {
+        (*matrix)[i][j] = ::distance(points[i], points[j]);
+        nmax = max(nmax, (*matrix)[i][j]);
+      }
     }
+    (*matrix)[i][i] = nelts * nmax;
+    (*vec)[i] = ::distance(make_pair(0, 0), points[i]);
+  }
+}
+
+void read_vector_of_points(int nelts, vector<pair<int, int> >* vec) {
+  for (int i = 0; i < nelts; i++) {
+    cin >> (*vec)[i].first >> (*vec)[i].second;
   }
 }
 
 int main(int argc, char** argv) {
-  int nrows, ncols, s;
+  int nelts;
+  scanf("%d", &nelts);
 
-  scanf("%d%d%d", &nrows, &ncols, &s);
+  vector<pair<int, int> > points(nelts);
+  read_vector_of_points(nelts, &points);
 
-  int** matrix = new int* [nrows];
-  for (int i = 0; i < nrows; i++) {
-    matrix[i] = new int[ncols];
-  }
+  vector<vector<double> > matrix(nelts, vector<double>(nelts));
+  vector<double> vec(nelts);
 
-  randmat(nrows, ncols, s, matrix);
+  outer(nelts, points, &matrix, &vec);
 
-  printf("%d %d\n", nrows, ncols);
-  for (int i = 0; i < nrows; i++) {
-    for (int j = 0; j < ncols; j++) {
-      printf("%d ", matrix[i][j]);
+  printf("%d %d\n", nelts, nelts);
+  for (int i = 0; i < nelts; i++) {
+    for (int j = 0; j < nelts; j++) {
+      printf("%g ", matrix[i][j]);
     }
     printf("\n");
   }
   printf("\n");
 
-  for (int i = 0; i < nrows; i++) {
-    delete[] matrix[i];
+  printf("%d\n", nelts);
+  for (int i = 0; i < nelts; i++) {
+    printf("%g ", vec[i]);
   }
-  delete[] matrix;
+  printf("\n");
 
   return 0;
 }
