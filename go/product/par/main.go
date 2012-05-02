61c7f6a776e6c3c21800b80a0ba50da89687d147
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-02 17:07:40 -0300

    go-product-par start

---
+++ go/product/par/main.go
@@ -0,0 +1,91 @@
+/*
+ * product: matrix-vector product
+ *
+ * input:
+ *   nelts: the number of elements
+ *   matrix: the real matrix
+ *   vector: the real vector
+ *
+ * output:
+ *   result: a real vector, whose values are the result of the product
+ */
+package main
+
+import (
+  "fmt"
+)
+
+type double float64;
+
+func product(nelts int, matrix [][]double, vector []double,
+    result []double) {
+  for i := 0; i < nelts; i++ {
+    var sum double = 0;
+    for j := 0; j < nelts; j++ {
+      sum += matrix[i][j] * vector[j];
+    }
+    result[i] = sum;
+  }
+}
+
+func read_integer() int {
+  var value int;
+  for true {
+    var read, _ = fmt.Scanf("%d", &value);
+    if read == 1 {
+      break;
+    }
+  }
+  return value;
+}
+
+func read_double() double {
+  var value double;
+  for true {
+    var read, _ = fmt.Scanf("%g", &value);
+    if read == 1 {
+      break;
+    }
+  }
+  return value;
+}
+
+func read_matrix(nelts int) [][]double {
+  var matrix [][]double;
+  matrix = make([][]double, nelts);
+  for i := 0; i < nelts; i++ {
+    matrix[i] = make([]double, nelts);
+    for j := 0; j < nelts; j++ {
+      matrix[i][j] = read_double();
+    }
+  }
+  return matrix;
+}
+
+func read_vector(nelts int) []double {
+  var vector []double;
+  vector = make([]double, nelts);
+  for i := 0; i < nelts; i++ {
+    vector[i] = read_double();
+  }
+  return vector;
+}
+
+func main() {
+  var nelts int;
+  var matrix [][]double;
+  var vector, result []double;
+
+  nelts = read_integer();
+  matrix = read_matrix(nelts);
+  vector = read_vector(nelts);
+  result = make([]double, nelts);
+
+  product(nelts, matrix, vector, result);
+
+  fmt.Printf("%d\n", nelts);
+  for i := 0; i < nelts; i++ {
+    fmt.Printf("%g ", result[i]);
+  }
+  fmt.Printf("\n");
+}
