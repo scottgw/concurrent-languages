710023ac20a94ccd1e4de55a102b63b4aff45908
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-02 17:13:32 -0300

    tbb-product-par done

--- tbb/product/par/main.cc
+++ tbb/product/par/main.cc
@@ -13,18 +13,28 @@
 #include <iostream>
 #include <vector>
 
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+
 using namespace std;
+using namespace tbb;
+
+typedef blocked_range<size_t> range;
 
 void product(int nelts, const vector<vector<double> >& matrix,
     const vector<double>& vec, vector<double>* result) {
 
-  for (int i = 0; i < nelts; i++) {
-    double sum = 0;
-    for (int j = 0; j < nelts; j++) {
-      sum += matrix[i][j] * vec[j];
-    }
-    (*result)[i] = sum;
-  }
+  parallel_for(
+    range(0, nelts),
+    [&](range r) {
+      for (size_t i = r.begin(); i != r.end(); ++i) {
+        double sum = 0;
+        for (int j = 0; j < nelts; j++) {
+          sum += matrix[i][j] * vec[j];
+        }
+        (*result)[i] = sum;
+      }
+  });
 }
 
 int main(int argc, char** argv) {
