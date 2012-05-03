4b3d01b0bed4ba48aa3e0da65defad6effa24d97
Kauê Soares da Silveira <silveira.kaue@gmail.com>
2012-05-02 21:28:43 -0300

    go-chain pause

---
+++ go/chain/main.go
@@ -0,0 +1,20 @@
+/*
+ * chain: chain all problems
+ *
+ * input:
+ *   nelts: the number of elements
+ *   randmat_seed: random number generator seed
+ *   thresh_percent: percentage of cells to retain
+ *   winnow_nelts: the number of points to select
+ *
+ * output:
+ *   result: a real vector, whose values are the result of the final product
+ *
+ */
+package main
+
+import (
+)
+
+func main() {
+}
