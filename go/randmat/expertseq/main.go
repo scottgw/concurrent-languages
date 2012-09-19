2ef05fafd6d9c83d8add06898de996734d2d786d
Scott West <scott.gregory.west@gmail.com>
2012-09-19 13:16:51 +0200

    go-randmat-expertseq pause

--- go/randmat/expertseq/main.go
+++ go/randmat/expertseq/main.go
@@ -12,60 +12,65 @@
 package main
 
 import (
-  "fmt"
-  "flag"
+	"flag"
+	"fmt"
 )
 
 type ByteMatrix struct {
-  Rows, Cols int
-  x []byte
+	Rows, Cols uint32
+	array      []byte
 }
 
-func NewByteMatrix (r, c int) *ByteMatrix {
-  return &ByteMatrix {r, c, make ([]byte, r*c)}
+func NewByteMatrix(r, c uint32) *ByteMatrix {
+	return &ByteMatrix{r, c, make([]byte, r*c)}
 }
 
-func (m *ByteMatrix) Row (i int) [] byte {
-  return m.X [i*m.Cols : (i+1)*m.Cols]
+func (m *ByteMatrix) Row(i uint32) []byte {
+	return m.array[i*m.Cols : (i+1)*m.Cols]
 }
 
-const(
-  LCG_A = 1664525
-  LCG_C = 1013904223
+const (
+	LCG_A = 1664525
+	LCG_C = 1013904223
 )
 
 var (
-  is_bench = flag.Bool("is_bench", false, "")
-  nrows    = flag.Int ("nrows", 10, "rows")
-  ncols   = flag.Int ("ncols", 9, "cols")
-  seed  =flag.Int ("seed", 8, "seed")
+	is_bench = flag.Bool("is_bench", false, "")
+	nrows    = flag.Uint("nrows", 10, "rows")
+	ncols    = flag.Uint("ncols", 9, "cols")
+	seed     = flag.Uint("seed", 8, "seed")
 )
 
-func randmat(nrows, ncols, s int) *ByteMatrix {
-  matrix := nNewByteMatrix (nrows, ncols)
+func randmat(nrows, ncols, s uint32) *ByteMatrix {
+	matrix := NewByteMatrix(nrows, ncols)
 
-  for i := 0; i < nrows; i++ {
-    seed := s + i;
-    row  := matrix.Row (i)
-    for j := range row {
-      seed = LCG_A * seed + LCG_C;
-      row [i] =  (seed % 100) % 100;
-    }
-  }
+	var i uint32
+	for i = 0; i < nrows; i++ {
+		var seed uint32
+		seed = s + i
+		row := matrix.Row(i)
+		for j := range row {
+			seed = LCG_A*seed + LCG_C
+			row[j] = byte(seed%100) % 100
+		}
+	}
+
+	return matrix
 }
 
 func main() {
-  flag.Parse();
+	flag.Parse()
 
-  randmat(nrows, ncols, s);
+	matrix := randmat(uint32(*nrows), uint32(*ncols), uint32(*seed))
 
-  if (!*is_bench) {
-    for i := 0; i < nrows; i++ {
-      for j := 0; j < ncols; j++ {
-        fmt.Printf("%d ", matrix[i][j]);
-      }
-      fmt.Printf("\n");
-    }
-    fmt.Printf("\n");
-  }
+	if !*is_bench {
+		for i := uint32(0); i < uint32(*nrows); i++ {
+			row := matrix.Row(i)
+			for j := range row {
+				fmt.Printf("%d ", row[j])
+			}
+			fmt.Printf("\n")
+		}
+		fmt.Printf("\n")
+	}
 }
