56bcd06b6bef194675ba7159dd7bf7c89ae1b5ac
Scott West <scott.gregory.west@gmail.com>
2012-09-23 23:18:19 +0200

    updating python script
    refactoring to take out manual t-tests and starting to integreate
    R statistical functions instead (so far just std-dev).

--- metric/perf/main.py
+++ metric/perf/main.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 import os
 import math
 import time
+from rpy import r
 
-from stats import *
 from problems import *
 from graphs import *
 from utils import *
@@ -14,8 +15,6 @@ problems = ["randmat","chain"]
 #variations = ["seq", "par"]
 variations = ["seq","expertpar", "par"]
 threads = [1, 2, 3, 4]
-ALPHA = 0.001
-GRAPH_SIZE = 700
 output_dir = "output"
 
 
@@ -35,11 +34,14 @@ class Config:
     self.inputs = inputs
     self.threads = threads
     self.all_values = all_values
-    self.alpha = ALPHA
     self.output_dir = output_dir
-    self.graph_size = GRAPH_SIZE
 
 cfg = Config ()
+
+class Data:
+  def __init__ (self, avg, std_dev):
+    self.avg = avg
+    self.std_dev = std_dev
 
 ERLANG_MAIN = ("#!/bin/sh\n"
                "cd ~/lucia/metric/perf/%s\n"
@@ -55,7 +57,6 @@ def get_all():
   for (problem, variation) in get_problems_with_variations():
     for language in sorted(languages):
       yield (language, problem, variation)
-
 
 def generate_erlang_main():
   for (problem, variation) in get_problems_with_variations():
@@ -86,7 +87,11 @@ def get_time_output(language, problem, variation, i, nthreads):
       language, problem, variation, i, nthreads)
 
 def is_sequential (variation):
-  return  variation == "seq" or variation == "expertseq"
+  return  variation.find('seq') >= 0
+
+def is_parallel (variation):
+  return  variation.find('par') >= 0
+
 
 def run_all(redirect_output=True):
   # TODO: check processor usage
@@ -95,12 +100,11 @@ def run_all(redirect_output=True):
       if is_sequential (variation) and nthreads != threads[-1]: continue
       for i in range(len(inputs)):
         #TODO: get time output file name
-        time_output = get_time_output(
-            language, problem, variation, i, nthreads)
+        time_output = get_time_output(language, problem, variation, i, nthreads)
         # TODO: refactor variations
         #print time_output
         cmd = ""
-        if language == "go" and (variation == 'par' or variation == 'expertpar'):
+        if language == "go" and is_parallel (variation):
           cmd += "GOMAXPROCS=%d " % nthreads
 
         if is_sequential (variation) or nthreads == 1:
@@ -164,174 +168,33 @@ def run_all(redirect_output=True):
         print tdiff
 
 results = {}
-INVALID = 999
 
 def get_results():
   for nthreads in threads:
     if nthreads not in results:
       results[nthreads] = {}
-      all_values[nthreads] = {}
     for (language, problem, variation) in get_all():
       if is_sequential (variation) and nthreads != threads[-1]: continue
       if problem not in results[nthreads]:
         results[nthreads][problem] = {}
-        all_values[nthreads][problem] = {}
       if variation not in results[nthreads][problem]:
         results[nthreads][problem][variation] = {}
-        all_values[nthreads][problem][variation] = {}
       if language not in results[nthreads][problem][variation]:
         results[nthreads][problem][variation][language] = {}
-        all_values[nthreads][problem][variation][language] = {}
 
       for i in range(len(inputs)):
-        results[nthreads][problem][variation][language][i] = INVALID
         time_output = get_time_output(
             language, problem, variation, i, nthreads)
         #print time_output
         cur = read_file_values(time_output)
-        all_values[nthreads][problem][variation][language][i] = cur
+        data = Data (r.mean (cur), r.sd (cur))
 
-        if (len(cur) == 0):
-          continue
-
-        total = 0.0
-        for j in range(len(cur)):
-          total += cur[j]
-        avg = total / len(cur)
-        print nthreads, problem, variation, language, i, avg
-        #if language == "erlang": # IMPORTANT !!!
-          #assert(avg > 1)
-          #avg -= 1
-        results[nthreads][problem][variation][language][i] = avg
-  #print results
-  #print all_values
-
-
-ttest_res = {}
-
-def test_significance_speedup():
-  for nthreads in threads:
-    if nthreads == 1:
-      continue
-    for (language, problem, variation) in get_all():
-      if is_sequential (variation): continue
-
-      for i in range(len(inputs)):
-        values = all_values[nthreads][problem][variation][language][i]
-        single = all_values[threads[-1]][problem]['seq'][language][i]
-        pvalue = ttest(single, values, ALPHA)
-
-        if not nthreads in ttest_res['spseq']:
-          ttest_res['spseq'][nthreads] = {}
-        if not problem in ttest_res['spseq'][nthreads]:
-          ttest_res['spseq'][nthreads][problem] = {}
-        if not variation in ttest_res['spseq'][nthreads][problem]:
-          ttest_res['spseq'][nthreads][problem][variation] = {}
-        if not language in ttest_res['spseq'][nthreads][problem][variation]:
-          ttest_res['spseq'][nthreads][problem][variation][language] = {}
-        ttest_res['spseq'][nthreads][problem][variation][language][i] = pvalue
-
-        previous = all_values[nthreads - 1][problem][variation][language][i]
-        pvalue = ttest(previous, values, ALPHA)
-
-        if not nthreads in ttest_res['spprev']:
-          ttest_res['spprev'][nthreads] = {}
-        if not problem in ttest_res['spprev'][nthreads]:
-          ttest_res['spprev'][nthreads][problem] = {}
-        if not variation in ttest_res['spprev'][nthreads][problem]:
-          ttest_res['spprev'][nthreads][problem][variation] = {}
-        if not language in ttest_res['spprev'][nthreads][problem][variation]:
-          ttest_res['spprev'][nthreads][problem][variation][language] = {}
-        ttest_res['spprev'][nthreads][problem][variation][language][i] = pvalue
-
-  for t in ['spseq', 'spprev']:
-    for nthreads in threads:
-      if nthreads == 1:
-        continue
-      for (language, problem, variation) in get_all():
-        if variation == 'seq': continue
-
-        for i in range(len(inputs)):
-          pvalue = ttest_res[t][nthreads][problem][variation][language][i]
-          passed = pvalue <= ALPHA
-          if not passed:
-            print 'nth%d:%s:%s:%s:in%d NOT passed %s' % (
-                nthreads, problem, variation, language, i, t)
-
-  pretty = {'spseq' : 'sequential', 'spprev' : 'previous parallel'}
-
-def test_significance():
-  ttest_types = ['spseq', 'spprev', 'seq', 'par']
-  for t in ttest_types:
-    ttest_res[t] = {}
-
-  test_significance_speedup()
-
-  print '\n\n*** sequential execution time ***\n\n'
-  for i in range(len(inputs)):
-    for problem in problems:
-      print '\n### %s ###' % problem
-      for la in languages:
-        print '%s is better than:' % la, 
-        values = all_values[threads[-1]][problem]['seq'][la][i]
-        for lb in languages:
-          if la != lb:
-            other = all_values[threads[-1]][problem]['seq'][lb][i]
-            pvalue = ttest(other, values, ALPHA)
-
-            if 'seq' not in ttest_res:
-              ttest_res['seq'] = {}
-            if problem not in ttest_res['seq']:
-              ttest_res['seq'][problem] = {}
-            if la not in ttest_res['seq'][problem]:
-              ttest_res['seq'][problem][la] = {}
-            if lb not in ttest_res['seq'][problem][la]:
-              ttest_res['seq'][problem][la][lb] = {}
-            ttest_res['seq'][problem][la][lb][i] = pvalue
-
-            passed = pvalue <= ALPHA
-            if passed:
-              print ' %s' % lb, 
-        print ''
-
-  print '\n\n*** parallel execution time ***\n\n'
-  for i in range(len(inputs)):
-    for problem in problems:
-      print '\n### %s ###' % problem
-      for la in languages:
-        print '%s is better than:' % la, 
-        values = all_values[threads[-1]][problem]['par'][la][i]
-        for lb in languages:
-          if la != lb:
-            other = all_values[threads[-1]][problem]['par'][lb][i]
-            pvalue = ttest(other, values, ALPHA)
-
-            if 'par' not in ttest_res:
-              ttest_res['par'] = {}
-            if problem not in ttest_res['par']:
-              ttest_res['par'][problem] = {}
-            if la not in ttest_res['par'][problem]:
-              ttest_res['par'][problem][la] = {}
-            if lb not in ttest_res['par'][problem][la]:
-              ttest_res['par'][problem][la][lb] = {}
-            ttest_res['par'][problem][la][lb][i] = pvalue
-
-            passed = pvalue <= ALPHA
-            if passed:
-              print ' %s' % lb, 
-        print ''
-
-  pretty = {'seq' : 'sequential'
-            , 'par' : 'parallel'
-            , 'expertseq' : 'expert sequential'
-            , 'expertpar' : 'expert parallel'
-            }
-
+        results[nthreads][problem][variation][language][i] = data
 
 def output_graphs():
-  read_table()
-  # create_graphs2(cfg,results[threads[-1]])
-  speedup_lang_var (cfg, results)  
+  create_graphs2(cfg, results[threads[-1]])
+  speedup_lang_var (cfg, results)
+  speedup_prob_var (cfg, results)
 #  create_graph(cfg, "exec-time", results[threads[-1]], "")
 #  create_graph(cfg, "exec-time", results[threads[-1]], "", is_relative=True)
 #  speedup_graph_name = 'speedup'
@@ -341,76 +204,13 @@ def output_graphs():
 
 TOTAL_EXECUTIONS = 3
 
-def calculate():
-  # float(results[threads[-1]][problem]['par'][language][0])))
-  nt = threads[-1]
-  i = 0
-  variations = ['par']
-  nmax = {}
-  nmin = {}
-  local_problems = []
-  for problem in problems:
-    if problem != 'product':
-      local_problems.append(problem)
-
-  for problem in local_problems:
-    nmax[problem] = {}
-    nmin[problem] = {}
-    for variation in variations:
-      nmax[problem][variation] = 0
-      nmin[problem][variation] = 999999999999
-      for language in languages:
-        nmax[problem][variation] = max(nmax[problem][variation],
-            results[nt][problem][variation][language][i])
-        nmin[problem][variation] = min(nmin[problem][variation],
-            results[nt][problem][variation][language][i])
-  calc = {}
-  nsum = 0.0
-  #for problem in local_problems:
-    #print problem, 
-  #print ''
-  for language in languages:
-    #print '%s:' % language, 
-    num = 0.0
-    den = 0.0
-    for problem in local_problems:
-      for variation in variations:
-        #print ' %f (%f) ' % (
-            #results[nt][problem][variation][language][i],
-            #results[nt][problem][variation][language][i] / float(
-                #nmin[problem][variation]))
-        num += (results[nt][problem][variation][language][i] / (
-            float(nmin[problem][variation])))
-        den += 1
-    #print ''
-    #print '%f / %f = %f' % (num, float(den), num / float(den))
-    calc[language] = num / float(den)
-    nsum += calc[language]
-
-  vec = []
-  for language in languages:
-    #vec.append((calc[language] / nsum, language))
-    vec.append((calc[language], language))
-
-  print 'time to execute (par %d)' % nt
-
-  vec.sort()
-  for x in vec:
-    (val, lang) = x
-    print '%s : %f' % (lang, val)
-
-
-
 def main():
-#  make_all()
-#  create_inputs()
-#  for _ in range(TOTAL_EXECUTIONS):
-#    run_all(redirect_output=False)  # TODO: remove outputs
+  make_all()
+  create_inputs()
+  for _ in range(TOTAL_EXECUTIONS):
+    run_all(redirect_output=False)  # TODO: remove outputs
   get_results()
   output_graphs()
-  # system('xmessage " ALL DONE " -nearmouse -timeout 1')
-  # raw_input("done! press enter to continue...")
-  # system('cd %s && make' % output_dir)
 
 if __name__ == '__main__':
   main()
