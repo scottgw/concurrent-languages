643cb322df3f8d16caab30eb63a9a5b85f6b677f
Scott West <scott.gregory.west@gmail.com>
2012-09-25 15:08:14 +0200

    updating graphs script to use rpy2 and ggplot2

--- metric/perf/graphs.py
+++ metric/perf/graphs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-from rpy import r 
-
 from problems import *
 from utils import *
 from config import *
 
-r.library ("gplots")
-r.library ("ggplot2")
+import rpy2.robjects as robjects
+import rpy2.robjects.lib.ggplot2 as ggplot2
+from rpy2.robjects.packages import importr
+from rpy2.robjects import FloatVector, StrVector, IntVector, DataFrame
+
 
 bargraph_dir = os.path.abspath("../time/graph")
 pretty_names = {"seq"      : "Sequential",
@@ -21,13 +22,6 @@ def main():
   hist_graphs(cfg, results[threads[-1]])
   speedup_lang_var (cfg, results)
   speedup_prob_var (cfg, results)
-#  create_graph(cfg, "exec-time", results[threads[-1]], "")
-#  create_graph(cfg, "exec-time", results[threads[-1]], "", is_relative=True)
-#  speedup_graph_name = 'speedup'
-#  create_speedup_graph(cfg, speedup_graph_name, results)
-#  create_problem_speedup_graph(cfg, "problem-speedup", speedup_graph_name)
-#  create_language_speedup_graph(cfg, "language-speedup", speedup_graph_name)
-
 
 def get_results():
   results = {}
@@ -46,7 +40,6 @@ def get_results():
       for i in range(len(inputs)):
         time_output = get_time_output(
             language, problem, variation, i, nthreads)
-        #print time_output
         cur = read_file_values(time_output)
 
         results[nthreads][problem][variation][language][i] = cur
@@ -64,47 +57,53 @@ def fieller (aa, bb):
   sem_b = r.sd (bb) / sqrt (len (bb))
 
   t   = r.qt (0.975, len (aa))
-  se_q = q * sqrt (sem_a^2/mean_a^2 + sem_b^2/mean_b^2)
+  se_q = q * sqrt (sem_a**2/mean_a**2 + sem_b**2/mean_b**2)
 
   return (q - t * se_q, q + t * se_q)
 
 
 def hist_graphs (cfg, values):
+  r = robjects.r
   for var in cfg.variations:
     # each variation gets plot
     avgs = []
-    cis = []
+    ses = []
+
+    langs = []
+    probs = []
 
     for prob in cfg.problems:
       # aggregate by problems
       for lang in cfg.languages:
         # each problem displays a list of language times for that problem
-        data = values[prob][var][lang][0]
+        data = FloatVector (values[prob][var][lang][0])
         
-        avgs.append (r.mean (data))
-        cis.append (r.t_test (data, **{"conf.level": 0.975}))
+        langs.append (lang)
+        probs.append (prob)
+        avgs.append (r['mean'] (data)[0])
 
-    def lang_matrix (v):
-      return r.matrix (v, len (cfg.languages), len (cfg.problems))
-
-    counts = lang_matrix (avgs)
-
-    # extract lower and upper bounds of confidence interval
-    lowers, uppers = zip (*map(lambda ci: ci ['conf.int'], cis))    
-    lower_bound = lang_matrix (lowers)
-    upper_bound = lang_matrix (uppers)
+        t_result = r['t.test'] (data, **{"conf.level": 0.975}).rx ('conf.int')[0]
+        ses.append ((t_result[1] - t_result[0])/2)
 
     r.pdf ('bargraph-time-' + var + '.pdf')
-    # r.barplot2 (counts, beside=True, names=cfg.problems, xpd=False, 
-    #             legend=cfg.languages,
-    #             **{"plot.grid" : True, "plot.ci": True, 
-    #                "ci.l": lower_bound, "ci.u": upper_bound})
 
-    # r.title (main = pretty_names [var] + ' execution time (seconds)')
-    r.ggplot (counts, 
-              r.aes (x="dose", y="len", fill="supp") +
-              r["geom_bar"] (position=r["position_dodge"]()))
-    r.dev_off()
+    df = robjects.DataFrame({'Language': StrVector (langs),
+                             'Problem': StrVector (probs),
+                             'Time' : FloatVector (avgs),
+                             'SE' : FloatVector (ses)
+                             })
+
+    limits = ggplot2.aes (ymax = 'Time + SE', ymin = 'Time - SE')
+    dodge = ggplot2.position_dodge (width=0.9)
+    gp = ggplot2.ggplot (df)
+
+    pp = gp + \
+        ggplot2.aes_string (x='Problem', y='Time', fill='Language') + \
+        ggplot2.geom_bar (position='dodge', stat='identity') + \
+        ggplot2.geom_errorbar (limits, position=dodge, width=0.25)
+ 
+    pp.plot ()
+    r['dev.off']()
 
 
 # produce a graph for each langauge/variation that shows 
@@ -118,7 +117,7 @@ def speedup_prob_var (cfg, values):
       for prob in cfg.problems:
         selector = lambda lang: lambda thread:\
             values [thread][prob][var][lang][0]
-        line_plot (cfg, var, prob, cfg.languages, selector)
+        line_plot (cfg, var, prob, 'Language', cfg.languages, selector)
 
 # produce a graph for each language/variation that shows all problems
 # speedups
@@ -128,30 +127,44 @@ def speedup_lang_var (cfg, values):
       for lang in cfg.languages:
         selector = lambda prob: lambda thread:\
             values [thread][prob][var][lang][0]
-        line_plot (cfg, var, lang, cfg.problems, selector)
+        line_plot (cfg, var, lang, 'Problem', cfg.problems, selector)
 
 
 # Function to plot lines for a given dataset. Here these
 # datasets will be either per-problem or per-language.
-def line_plot (cfg, var, control, changing, selector):
-  r.pdf ('speedup-' + var + '-' + control + '.pdf')
-  r.plot (cfg.threads, cfg.threads, type='l',
-          yaxp = (0, max(cfg.threads), 8),
-          ylim=(0, max(cfg.threads)*1.15))
+def line_plot (cfg, var, control, change_name, changing, selector):
+  r = robjects.r
 
-  r.lines (cfg.threads, cfg.threads)
+  r.pdf ('speedup-' + var + '-' + control + '.pdf')
+
+  speedups = []
+  thrds = []
+  changes = []
 
   for c in changing:
-    cluster = []
     sel     = selector (c)
     thr_1_data = sel(1)
 
     for n in cfg.threads:
-      cluster.append (r.mean (sel(1)) / r.mean (sel(n)))
-    r.lines (cfg.threads, cluster, type='o', pch=changing.index (c))
+      m1 = r.mean (FloatVector (sel(1)))[0]
+      mn = r.mean (FloatVector (sel(n)))[0]
+      speedups.append (m1 / mn)
+      thrds.append (n)
+      changes.append (c)
 
-  r.legend ("topright", None, legend=changing, pch=range(len(changing)))
-  r.dev_off()
+  df = DataFrame ({'Speedup': FloatVector (speedups),
+                   'Threads': IntVector (thrds),
+                   change_name: StrVector (changes)
+                   })
+
+  gg = ggplot2.ggplot (df)
+  pp = gg + ggplot2.geom_line() + \
+      ggplot2.aes_string(x='Threads', y='Speedup', 
+                         group=change_name, color=change_name)
+
+  pp.plot()
+
+  r['dev.off']()
 
 if __name__ == '__main__':
   main ()
