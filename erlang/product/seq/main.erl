402e5feeff3abfe9434280eaa17c3742715e26aa
Scott West <scott.gregory.west@gmail.com>
2012-10-04 05:15:07 +0200

    updating erlang product for benchmark mode

--- erlang/product/seq/main.erl
+++ erlang/product/seq/main.erl
@@ -11,22 +11,29 @@
 %
 
 -module(main).
--export([main/0]).
+-export([main/0, main/1]).
 
 product(_, Matrix, Vector) ->
   [ lists:sum([ A * B || {A, B} <- lists:zip(L, Vector)]) || L <- Matrix].
 
-read_vector(0) -> [];
-read_vector(Nelts) -> {ok, [X]} = io:fread("", "~f"),
-  [ X | read_vector(Nelts - 1)].
+read_vector(IsBench, 0) -> [];
+read_vector(IsBench, Nelts) -> 
+    Val = if IsBench -> 0;
+             true -> {ok, [X]} = io:fread("", "~f"), X
+          end,
+    [ Val | read_vector(IsBench, Nelts - 1)].
 
-read_matrix(0, _) -> [];
-read_matrix(Nelts, Total) ->
-  [ read_vector(Total) | read_matrix(Nelts - 1, Total)].
+read_matrix(IsBench, 0, _) -> [];
+read_matrix(IsBench, Nelts, Total) ->
+  [ read_vector(IsBench, Total) | read_matrix(IsBench, Nelts - 1, Total)].
 
-main() ->
+
+main() -> main(['']).
+main(Args) ->
+  [Head | _] = Args,
+  IsBench = string:equal(Head, 'is_bench'),
   {ok, [Nelts]} = io:fread("","~d"),
-  Matrix = read_matrix(Nelts, Nelts),
-  Vector = read_vector(Nelts),
+  Matrix = read_matrix(IsBench, Nelts, Nelts),
+  Vector = read_vector(IsBench, Nelts),
   io:format("~w~n\n", [product(Nelts, Matrix, Vector)]).
 
